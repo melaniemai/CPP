e27e4f5250987195822d60e084045cd0be65fa96
melaniemai <44130793+melaniemai@users.noreply.github.com>
2019-08-07 00:28:15 -0400

    Add files via upload

---
+++ lab9/quad.cpp
@@ -0,0 +1,187 @@
+//// Melanie Mai - DONE
+
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//#include <cctype>
+//#include <cmath>
+
+//using namespace std;
+
+//// Please replace this Point class with your version from Part B
+//class Point {
+
+//    // cout implementation for Point class
+//    friend ostream & operator<<( ostream &output, const Point &P ){
+//        output << "x: " << P.x << " y: " << P.y;
+//        return output;
+//    }
+
+//    // cin implementation for Point class
+//    friend istream & operator>>( istream &input, Point &P ){
+//        input >> P.x >> P.y;
+//        return input;
+//    }
+
+//public:
+//    // Default constructor
+//    Point(){
+//        x = 0;
+//        y = 0;
+//    }
+
+//    // Override constructor
+//    Point(double in_x, double in_y){
+//        x = in_x;
+//        y = in_y;
+//    }
+
+//    // Setter
+//    double setXY(double in_x, double in_y){
+//        x = in_x;
+//        y = in_y;
+//    }
+
+//    // Getter, get value of x to use from private
+//    double getX(){
+//        return x;
+//    }
+
+//    // Getter, get value of y to use from private
+//    double getY(){
+//        return y;
+//    }
+
+//    Point operator + (Point P_in){
+//        return { x + P_in.getX(), y + P_in.getY()};
+//    }
+
+//    bool operator==(Point &rhs) {
+//        return (rhs.x == x && rhs.y == y);
+//    }
+
+//private:
+//    int x,y;
+//};
+
+//class Quad {
+//    // implement cout for Line classes objects
+//    friend ostream & operator<<( ostream &output, const Quad &Q ){
+//        output << "Point 1 (a): " << Q.P1 << endl << "Point 2 (b): " << Q.P2 << endl << "Point 3 (c): " << Q.P3 << endl
+//               << "Point 4 (d): " << Q.P4;
+//        return output;
+//    }
+
+//    // implement cin for Line classes objects
+//    friend istream & operator>>( istream &input, Quad &Q ){
+//        input >> Q.P1 >> Q.P2 >> Q.P3 >> Q.P4;
+//        return input;
+//    }
+
+//public:
+
+//    // Default Constructor
+//    Quad(){
+//        Point P1(0,0);
+//        Point P2(0,0);
+//        Point P3(0,0);
+//        Point P4(0,0);
+//    }
+
+//    // Override constructor
+//    Quad(Point aP1, Point aP2, Point aP3, Point aP4){
+//        P1 = aP1;
+//        P2 = aP2;
+//        P3 = aP3;
+//        P4 = aP4;
+//    }
+
+//    // Sets argument points to P1 and P2
+//    void setXY2(Point& aP1, Point& aP2, Point& aP3, Point& aP4){
+//        P1 = aP1;
+//        P2 = aP2;
+//        P3 = aP3;
+//        P4 = aP4;
+//    }
+
+//    // Get P1 value
+//    Point getP1(){
+//        return P1;
+//    }
+
+//    // Get P2 value
+//    Point getP2(){
+//        return P2;
+//    }
+
+//    // Get P3 value
+//    Point getP3(){
+//        return P3;
+//    }
+
+//    // Get P4 value
+//    Point getP4(){
+//        return P4;
+//    }
+
+//    // Collinearity test (Determines if object is a quadrilateral or not)
+//    double collinearitytest(){
+//        double triad1, triad2, triad3, triad4;
+//        triad1 = P1.getX()*(P2.getY()-P3.getY()) + P2.getX()*(P3.getY()-P1.getY())+ P3.getX()*(P1.getY()-P2.getY());
+//        triad2 = P2.getX()*(P3.getY()-P4.getY()) + P3.getX()*(P4.getY()-P2.getY()) + P4.getX()*(P2.getY() - P3.getY());
+//        triad3 = P1.getX()*(P3.getY()-P4.getY())+ P3.getX()*(P4.getY()-P1.getY()) + P4.getX()*(P3.getY()-P1.getY());
+//        triad4 = P1.getX()*(P2.getY()-P4.getY())+ P2.getX()*(P4.getY()-P1.getY()) + P4.getX()*(P1.getY()-P2.getY());
+//        if(compareFloats(triad1, 0) || compareFloats(triad2, 0) || compareFloats(triad3, 0) || compareFloats(triad4, 0)){
+//            return false; // quad cannot be formed
+//        } else{
+//            return true;
+//        }
+//    }
+
+//    // Calculate quad area
+//    double quadArea(){
+//        double a = 0.0; double aa = 0.0;
+//        // Given formula
+//        a = fabs((P1.getX()*P2.getY())+(P2.getX()*P3.getY()) + (P3.getX()*P4.getY())+(P4.getX()*P1.getY())-(P2.getX()*P1.getY())-(P3.getX()*P2.getY())-(P4.getX()*P3.getY())-(P1.getX()*P4.getY()));
+//        aa = 0.5 * a;
+//        return aa;
+//    }
+
+//    // Display info
+//    void display(){
+//        if(collinearitytest() == false){
+//            cout << "Not a quadrilateral." << endl;
+//        } else{
+//            cout << "Object is a quadrilateral." << endl;
+//            cout << "Area: " << quadArea() << endl;
+//        }
+//    }
+
+//    // Compare floating point numbers
+//    bool compareFloats(double firstFloat, double secondFloat, double epsilonV = 0.000006){
+//        return (fabs(firstFloat - secondFloat) < epsilonV);
+//    }
+
+//private:
+//    Point P1, P2, P3, P4;
+//};
+
+//int main(){
+//    Quad Q1; Quad Q2;
+//    // Quad L2(5,6,7,8);
+
+//    cout << "Enter Four Points (Q1): ";
+//    cin >> Q1;
+//    cout << Q1 << endl;
+//    Q1.display();
+
+//    cout << endl;
+
+//    cout << "Enter Four Points (Q2): ";
+//    cin >> Q2;
+//    cout << Q2 << endl;
+//    Q2.display();
+
+
+//    return 0;
+//}
---
+++ lab9/triangle.cpp
@@ -0,0 +1,245 @@
+//// Melanie Mai - DONE
+
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//#include <cctype>
+//#include <cmath>
+
+//using namespace std;
+
+//const double epsilon = 0.0000001;
+
+//// Please replace this Point class with your version from Part B
+//class Point {
+
+//   // cout implementation for Point class
+//   friend ostream & operator<<( ostream &output, const Point &P ){
+//       output << "x: " << P.x << " y: " << P.y;
+//       return output;
+//   }
+
+//   // cin implementation for Point class
+//   friend istream & operator>>( istream &input, Point &P ){
+//       input >> P.x >> P.y;
+//       return input;
+//   }
+
+//public:
+//   // Default constructor
+//   Point(){
+//       x = 0;
+//       y = 0;
+//   }
+
+//   // Override constructor
+//   Point(double in_x, double in_y){
+//       x = in_x;
+//       y = in_y;
+//   }
+
+//   // Setter
+//   double setXY(double in_x, double in_y){
+//       x = in_x;
+//       y = in_y;
+//   }
+
+//   // Getter, get value of x to use from private
+//   double getX(){
+//       return x;
+//   }
+
+//   // Getter, get value of y to use from private
+//   double getY(){
+//       return y;
+//   }
+
+//   Point operator + (Point P_in){
+//       return { x + P_in.getX(), y + P_in.getY()};
+//   }
+
+//   bool operator==(Point &rhs) {
+//       return (rhs.x == x && rhs.y == y);
+//   }
+
+//private:
+//   double x,y;
+//};
+
+//class Triangle {
+//   // implement cout for Line classes objects
+//   friend ostream & operator<<( ostream &output, const Triangle &T ){
+//       output << "Point 1 (a): " << T.P1 << endl << "Point 2 (b): " << T.P2 << endl << "Point 3 (c): " << T.P3;
+//       return output;
+//   }
+
+//   // implement cin for Line classes objects
+//   friend istream & operator>>( istream &input, Triangle &T ){
+//       input >> T.P1 >> T.P2 >> T.P3;
+//       return input;
+//   }
+
+//public:
+
+//   // Default Constructor
+//   Triangle(){
+//       Point P1(0,0);
+//       Point P2(0,0);
+//       Point P3(0,0);
+//   }
+
+//   // Override constructor
+//   Triangle(Point aP1, Point aP2, Point aP3){
+//       P1 = aP1;
+//       P2 = aP2;
+//       P3 = aP3;
+//   }
+
+//   // Sets argument points to P1 and P2
+//   void setTriangle(Point& aP1, Point& aP2, Point& aP3){
+//       P1 = aP1;
+//       P2 = aP2;
+//       P3 = aP3;
+//   }
+
+//   // Get P1 value
+//   Point getP1(){
+//       return P1;
+//   }
+
+//   // Get P2 value
+//   Point getP2(){
+//       return P2;
+//   }
+
+//   // Get P3 value
+//   Point getP3(){
+//       return P3;
+//   }
+
+//   bool operator == (Triangle &rhs){
+//       return (rhs.P1 == P1 && rhs.P2 == P2 && rhs.P3 == P3);
+//   }
+
+
+//   // Determine if object is a triangle
+//   bool isObjectTriangle(){
+//       double tri;
+//       tri = P1.getX()*(P2.getY()-P3.getY())+ P2.getX()*(P3.getY()-P1.getY()) + P3.getX()*(P1.getY()-P2.getY());
+//       if(tri == 0.0){
+//           return false;
+//       }else{
+//           return true;
+//       }
+//   }
+
+//   // Calculate area of a triangle
+//   double area(){
+//       double a = 0.0;
+//       double l1, l2, l3;
+//       double pp = 0.0;
+//       if(isObjectTriangle() == false){
+//           return a;
+//       } else{
+//           pp = perimeter() / 2;
+//           l1 = sqrt(pow((P2.getX() - P1.getX()), 2) + pow((P2.getY() - P1.getY()), 2));
+//           l2 = sqrt(pow((P3.getX() - P2.getX()), 2) + pow((P3.getY() - P2.getY()), 2));
+//           l3 = sqrt(pow((P3.getX() - P1.getX()), 2) + pow((P3.getY() - P1.getY()), 2));
+//           a = sqrt(pp*(pp-l1)*(pp-l2)*(pp-l3));
+//       }
+//       return a;
+//   }
+
+//   // Calculate perimeter of a triangle
+//   double perimeter(){
+//       double p = 0.0;
+//       double l1, l2, l3;
+//       if(isObjectTriangle() == false){
+//           return p;
+//       } else {
+//           l1 = sqrt(pow((P2.getX() - P1.getX()), 2) + pow((P2.getY() - P1.getY()), 2));
+//           l2 = sqrt(pow((P3.getX() - P2.getX()), 2) + pow((P3.getY() - P2.getY()), 2));
+//           l3 = sqrt(pow((P3.getX() - P1.getX()), 2) + pow((P3.getY() - P1.getY()), 2));
+//           p = l1 + l2 + l3;
+//       }
+//       return p;
+//   }
+
+//   // Determine if triangle is equilateral triangle
+//   bool isEquilateralTriangle(){
+//       double l1, l2, l3;
+//       if(isObjectTriangle() == false){
+//           return false;
+//       } else{
+//           l1 = sqrt(pow((P2.getX() - P1.getX()), 2) + pow((P2.getY() - P1.getY()), 2));
+//           l2 = sqrt(pow((P3.getX() - P2.getX()), 2) + pow((P3.getY() - P2.getY()), 2));
+//           l3 = sqrt(pow((P3.getX() - P1.getX()), 2) + pow((P3.getY() - P1.getY()), 2));
+//           if((l1 == l2) && (l2 == l3) && (l3 == l1)){
+//               return true;
+//           } else{
+//               return false;
+//           }
+//       }
+//   }
+
+//   // Determine if both triangles are the same
+//   bool isBothTrianglesSame(Triangle x, Triangle y){
+//       if(x == y){
+//           return true;
+//       } else{
+//           return false;
+//       }
+//   }
+
+//   // Display info
+//   void display(){
+//       if(isObjectTriangle() == false){
+//           cout << "Not a triangle." << endl;
+//           cout << "Area: " << area() << endl;
+//           cout << "Perimeter: " << perimeter() << endl;
+//       } else {
+//           cout << "Object is a triangle." << endl;
+//           if(isEquilateralTriangle() == true){
+//               cout << "Object is an Equilateral Triangle." << endl;
+//           } else{
+//               cout << "Object is not an Equilateral Triangle." << endl;
+//           }
+//           cout << "Area: " << area() << endl;
+//           cout << "Perimeter: " << perimeter() << endl;
+
+//       }
+//   }
+
+//private:
+//   Point P1, P2, P3;
+//};
+
+//int main(){
+//   Triangle T1; Triangle T2;
+//   // Triangle L2(5,6,7,8);
+
+//   cout << "Enter Three Points: ";
+//   cin >> T1;
+//   cout << T1 << endl;
+//   T1.display();
+
+
+
+//   cout << "Enter another Three Points: ";
+//   cin >> T2;
+//   cout << T2 << endl;
+//   T2.display();
+
+//   cout << endl;
+
+//   // Check if T1 and T2 are triangles to check if they are the same
+//   if(T1.isObjectTriangle() == true && (T2.isObjectTriangle() == true)){
+//       if(T2.isBothTrianglesSame(T1, T2) == true){
+//           cout << "Triangles are the same." << endl;
+//       }
+//   } else{
+//       cout << "Cannot compare triangles." << endl;
+//   }
+
+//   return 0;
+//}
