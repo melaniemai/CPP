283acb1fca0c11ea53ad084bd0bf4f18ade229e8
melaniemai <44130793+melaniemai@users.noreply.github.com>
2019-10-24 00:32:03 -0400

    Update l6.cpp

--- CS2028/labs/lab06/l6.cpp
+++ CS2028/labs/lab06/l6.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
 #include <string>
+#include <exception>
 
 using namespace std;
 
+class StackOverflow : public std::exception{
+public:
+  const char * what() const throw() override{
+    return "Stack overflow.";
+  }
+};
+
+class StackUnderflow : public std::exception{
+public:
+  const char * what() const throw() override{
+    return "Stack underflow.";
+  }
+};
+
+struct Moves{
+  int origin, destination;
+};
+
+template<class S>
+class Stack{
+private:
+  int arraySize;
+  int headValue;
+  S **stack;
+public:
+  Stack();
+  Stack(int size);
+  int getArraySize();
+  void push(S *ptr);
+  S *pop();
+  S *top();
+  int length();
+  void empty();
+  bool isFull();
+  
+  friend void displayTower(Stack<int> &tower);
+}
